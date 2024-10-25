1f176e9782a4d1e57179d8ca0ddaa22e611f2d75
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-10-25 21:53:21 +0500

    add signin page and init dashboard page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useDebounceCallback } from "usehooks-ts";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import { signInSchema } from "@/schemas/signInSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+
+const page = () => {
+    const { toast } = useToast();
+    const router = useRouter();
+
+    //zod implementation
+
+    const form = useForm<z.infer<typeof signInSchema>>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            identifier: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+        const result = await signIn("credentials", {
+            redirect: false,
+            identifier: data.identifier,
+            password: data.password,
+        });
+
+        if (result?.error) {
+            toast({
+                title: "Login Failed",
+                description: "Incorrect email or password",
+                variant: "destructive",
+            });
+        }
+
+        if (result?.url) {
+            router.replace("/dashboard");
+        }
+    };
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-800">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+                <div className="text-center">
+                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                        Get Started
+                    </h1>
+                    <p className="mb-4">
+                        Sign in to start your anonymous adventure
+                    </p>
+                </div>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-6"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="identifier"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="johndoe@jd.com"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            placeholder="mypassword123"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Sign In</Button>
+                    </form>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default page;
